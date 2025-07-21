17673c6cae2cb349de1ecb66f651aba2518986ae
Carlos16S <carlosanjs16@gmail.com>
2025-07-21 01:12:26 -0600

    Nueva conexion base de datos, se añadio un nueo requeimiento

--- config.py
+++ config.py
@@ -1,23 +1,45 @@
 ##Conexión a la base de datos
 
-import pyodbc
+import os
+import psycopg2
+
 class DB:
-    server = "CAJS2004-LAPTOP\SQLEXPRESS"
-    database = "Escuela_Musica"
-    username = "EscuAdmin"                                   #Parametros para la conexion a la base de datos
-    password = "Lopvyx82!@#"
-    driver = "ODBC Driver 17 for SQL Server"
-
-
     def __init__(self):
-      try:
-        self.conn = pyodbc.connect(
-            f"DRIVER={{{self.driver}}};SERVER={self.server};DATABASE={self.database};UID={self.username};PWD={self.password};TrustServerCertificate=yes"
-        )
-        print("✅ Conexión exitosa a SQL Server")
-        
-      except Exception as e:
-        print("❌ Error de conexión:", e)
+        try:
+            self.conn = psycopg2.connect(
+                "host=dpg-d1usi3umcj7s73eqg9mg-a dbname=escuela_musica user=escu_admin password=qZRHs24K31Yjy5qmEFVtpgZnYhpYekxw port=5432"
+            )
+            print("✅ Conexión exitosa a PostgreSQL")
+        except Exception as e:
+            print("❌ Error de conexión:", e)
 
 db = DB()
+
+
+
+
+
+
+
+
+#import pyodbc
+#class DB:
+#    server = "CAJS2004-LAPTOP\SQLEXPRESS"
+#    database = "Escuela_Musica"
+#    username = "EscuAdmin"                                   #Parametros para la conexion a la base de datos
+#    password = "Lopvyx82!@#"
+#    driver = "ODBC Driver 17 for SQL Server"
+#
+#
+#    def __init__(self):
+#      try:
+#        self.conn = pyodbc.connect(
+#            f"DRIVER={{{self.driver}}};SERVER={self.server};DATABASE={self.database};UID={self.username};PWD={self.password};TrustServerCertificate=yes"
+#        )
+#        print("✅ Conexión exitosa a SQL Server")
+#        
+#      except Exception as e:
+#        print("❌ Error de conexión:", e)
+#
+#db = DB()
         
